cb5cdfe1b86b0aaa9698fa7f6a763fff4c6a5073
Narayan Pal <narayan220602@gmail.com>
2026-03-15 16:13:48 +0530

    import shadcn card and wrap the login/signup components inside card

--- src/components/auth/auth.tsx
+++ src/components/auth/auth.tsx
@@ -1,19 +1,56 @@
 "use client";
 
+import { useState } from "react";
 import CustomInput from "../CustomInput/CustomInput";
 import { Button } from "../ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 type AuthProps = {
   type: "login" | "signup";
 };
 const Auth = ({ type }: AuthProps) => {
   console.log("Type ", type);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   return (
-    <div>
-      <CustomInput label="Email" value="" onChange={() => {}} placeholder="Enter your email"/>
-      <CustomInput label="Password" value="" onChange={() => {}} placeholder="Enter your Password"/>
-        <Button>{ type === 'login' ? 'Continue' : 'Get Started'}</Button>
-    </div>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>
+          {type === "login" ? "Login to your account" : "Create your account"}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="mb-4">
+          <CustomInput
+            label="Email"
+            value={email}
+            onChange={setEmail}
+            placeholder="Enter your email"
+            type="email"
+          />
+        </div>
+        <div>
+          <CustomInput
+            label="Password"
+            value={password}
+            onChange={setPassword}
+            placeholder="Enter your password"
+            type="password"
+          />
+        </div>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        <Button className="w-full">{type === "login" ? "Continue" : "Get Started"}</Button>
+      </CardFooter>
+    </Card>
   );
 };
 
