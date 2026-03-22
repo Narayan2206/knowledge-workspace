34291ad9bd44a13e16b8de5bd055440b3d1f8da9
Narayan Pal <narayan220602@gmail.com>
2026-03-22 22:17:17 +0530

    create models for tables

---
+++ src/lib/supabase/models.ts
@@ -0,0 +1,43 @@
+export interface Users {
+    id: string;
+    name: string;
+    email: string;
+    avatar: null | string;
+    created_at: string;
+}
+
+export interface Workspaces {
+    id: string;
+    name: string;
+    slug: string;
+    created_by: string;
+    created_at: string;
+}
+
+export interface WorkspaceMembers {
+    id: string;
+    workspace_id: string;
+    user_id: string;
+    joined_at: string;
+    role: 'owner' | 'admin' | 'editor' | 'viewer';
+}
+
+export interface Documents {
+    id: string;
+    workspace_id: string;
+    title: string;
+    content: string;
+    created_by: string;
+    updated_at: string;
+    created_at: string;
+    is_archived: boolean;
+}
+
+export interface DocumentVersions {
+    id: string;
+    document_id: string;
+    content: string;
+    created_by: string;
+    created_at: string;
+}
+
