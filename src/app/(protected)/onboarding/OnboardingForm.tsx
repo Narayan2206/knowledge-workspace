51364f207ec98cf14f8121f6bae227c7d04a2a4c
Narayan Pal <narayan220602@gmail.com>
2026-03-29 20:07:18 +0530

    create first workspace after onboarding

--- src/app/(protected)/onboarding/OnboardingForm.tsx
+++ src/app/(protected)/onboarding/OnboardingForm.tsx
@@ -2,20 +2,43 @@
 
 import CustomInput from "@/components/CustomInput/CustomInput";
 import { Button } from "@/components/ui/button";
+import { workspaceService } from "@/lib/services";
+import { useAuthStore } from "@/store/auth.store";
 import { useState } from "react";
 
 function OnboardingForm() {
   const [workspaceName, setWorkspaceName] = useState("");
+  const user = useAuthStore((s) => s.user);
 
-    function handleSubmit(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-        throw new Error("Function not implemented.");
+  const handleCreateWorkspace = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ): Promise<void> => {
+    e.preventDefault();
+
+    if(!user){
+        alert('User is not authenticated');
+        return;
     }
 
+    try {
+      const response = await workspaceService.createWorkspace({
+        name: workspaceName,
+        slug: `${Date.now()}-${Math.random()}`,
+        created_by: user.id,
+      });
+      console.log("Response ", response);
+    } catch (error) {
+        console.error(error);
+        alert('Unable to Create workspace');
+    }
+  };
+
   return (
-    <div>
-      <p className="mb-4">Welcome 👋! Create your workspace to get started.</p>
-      <div>
-        <div className="mb-4">
+    <div className="flex flex-col items-center gap-8 basis-10/12 md:basis-1/3">
+      <p>Welcome! 👋</p>
+      <p> Create your workspace to get started.</p>
+      <div className="w-full">
+        <div className="mb-8">
           <CustomInput
             label="Workspace Name"
             value={workspaceName}
@@ -25,12 +48,12 @@ function OnboardingForm() {
             required
           />
         </div>
-        <Button 
-        className="w-full"
-        onClick={handleSubmit}
-        // disabled={loading}
+        <Button
+          className="w-full"
+          onClick={handleCreateWorkspace}
+          disabled={workspaceName.length < 3}
         >
-            Create Workspace!
+          Create Workspace!
         </Button>
       </div>
     </div>
