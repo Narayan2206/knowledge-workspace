92609d29ac8f60dbd84381cf6f430f441b5969b7
Narayan Pal <narayan220602@gmail.com>
2026-03-30 00:04:08 +0530

    add loader to /authcallback and use supabase.auth.getUser instead of getClaims to fetch valid user

--- src/app/(public)/authcallback/page.tsx
+++ src/app/(public)/authcallback/page.tsx
@@ -3,25 +3,42 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/lib/supabase/client";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function AuthCallback() {
   const router = useRouter();
   const supabase = createClient();
 
   useEffect(() => {
+    let isMounted = true;
     const handleAuth = async () => {
-      const { data } = await supabase.auth.getClaims();
-      console.log("GET CLAIMS DATA ",  data);
-      
-      if (data) {
-        router.replace("/onboarding");
-      } else {
-        router.replace("/login");
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.getUser();
+      console.log("USER DATA ", user);
+
+      if (isMounted) {
+        if (error || !user) {
+          router.replace("/login");
+        } else {
+          router.replace("/onboarding");
+        }
       }
     };
 
     handleAuth();
-  }, []);
+    return () => {
+      isMounted = false;
+    };
+  }, [router, supabase]);
 
-  return <p>Loading...</p>;
+  return (
+    <main className="h-screen">
+      <div className="flex flex-col gap-2 items-center justify-center h-full">
+        <Spinner className="size-16" />
+        <p>Verifying, please wait...</p>
+      </div>
+    </main>
+  );
 }
