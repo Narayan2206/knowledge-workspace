7a5c40154652e9b311ef8ab501534bf440078b3f
Narayan Pal <narayan220602@gmail.com>
2026-03-29 20:16:35 +0530

    useWorkspace hook under progress

--- src/hooks/useWorkspace.ts
+++ src/hooks/useWorkspace.ts
@@ -1,13 +1,27 @@
 "use client";
 
+import { workspaceService } from "@/lib/services";
 import { useState } from "react";
 
 
 export function useWorkspace(){
     const [loading, setLoading] = useState(true);
 
-    function createWorkspace(){
+    function createWorkspace(workspaceData: {
+        name: string,
+        slug: string,
+        userId: string
+    }){
         // call workspaceService.createWorkspace
+        try {
+            const response = workspaceService.createWorkspace({
+                name: workspaceData.name,
+                slug: workspaceData.slug,
+                created_by: workspaceData.userId
+            })
+        } catch (error) {
+            
+        }
     }
 
     return {loading, createWorkspace}
