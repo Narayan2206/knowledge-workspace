52191c4c12c4b58a6b7271b3b5310e9b80659f3b
Narayan Pal <narayan220602@gmail.com>
2026-04-12 15:12:40 +0530

    refactor logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,32 @@ import { Button } from "@/components/ui/button";
 import { logOut } from "@/features/auth";
 import { useAuthStore } from "@/store/auth.store";
 import Link from "next/link";
+import { toast } from "sonner";
 
 export default function Home() {
   const user = useAuthStore((s) => s.user);
   const handleLogout = async () => {
-    const error = await logOut();
-    if (error) {
-      console.error("Error logging out user ", error);
-    } else {
-      console.log("User logged out successfully");
+    try {
+      await logOut();
+      toast.success("Logged out successfully");
+    } catch (error) {
+      console.error("Error logging out ", error);
+      toast.error("Error logging out");
     }
   };
   return (
     <header>
-    <div>
-      {user ? (
-        <Button onClick={handleLogout}>Logout</Button>
-      ) : (
-        <>
-          <Link href={"/login"}>Login</Link>
-          <Link href={"/signup"}>Signup</Link>
-        </>
-      )}
-    </div>
-    {user && <p>Welcome {user?.user_metadata?.email}</p>}
+      <div>
+        {user ? (
+          <Button onClick={handleLogout}>Logout</Button>
+        ) : (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link href={"/signup"}>Signup</Link>
+          </>
+        )}
+      </div>
+      {user && <p>Welcome {user?.user_metadata?.email}</p>}
     </header>
   );
 }
