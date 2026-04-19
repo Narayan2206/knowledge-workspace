c2730e80a0b83b39c796a187ff3f75727135e6c9
Narayan Pal <narayan220602@gmail.com>
2026-04-19 14:34:25 +0530

    wrap the shadcn sidebar inside a sidebar component

--- src/app/(protected)/workspace/[slug]/layout.tsx
+++ src/app/(protected)/workspace/[slug]/layout.tsx
@@ -1,37 +1,5 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { NavActions } from "@/components/nav-actions";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import BreadcrumbComponent from "./BreadcrumbComponent";
+import SidebarComponent from "@/components/Sidebar/SidebarComponent";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-14 shrink-0 items-center gap-2">
-          <div className="flex flex-1 items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-            />
-            <BreadcrumbComponent />
-          </div>
-          <div className="ml-auto px-3">
-            <NavActions />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 px-4 py-10">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <SidebarComponent>{children}</SidebarComponent>;
 }
---
+++ src/components/Sidebar/SidebarComponent.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { AppSidebar } from "@/components/app-sidebar";
+import { NavActions } from "@/components/nav-actions";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import BreadcrumbComponent from "./BreadcrumbComponent";
+
+export default function SidebarComponent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+    return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-14 shrink-0 items-center gap-2">
+          <div className="flex flex-1 items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+            />
+            <BreadcrumbComponent />
+          </div>
+          <div className="ml-auto px-3">
+            <NavActions />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 px-4 py-10">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+
+}
