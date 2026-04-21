95c960c6a617427b8ec9c279d4f474bb6a575378
Narayan Pal <narayan220602@gmail.com>
2026-04-21 21:53:46 +0530

    fetch documents along with workspace data

--- src/app/(protected)/workspace/[slug]/WorkspaceComponent.tsx
+++ src/app/(protected)/workspace/[slug]/WorkspaceComponent.tsx
@@ -1,40 +1,45 @@
 "use client";
 
 import Tiptap from "@/components/Editor/Tiptap";
-import { workspaceService } from "@/lib/services";
+import { workspaceDocumentService, workspaceService } from "@/lib/services";
 import { useWorkspaceStore } from "@/store/workspace.store";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Content } from "@tiptap/react";
+import { useDocumentStore } from "@/store/document.store";
 
 
 const WorkspaceComponent = () => {
   const params = useParams<{ slug: string }>();
   const { setIsLoadingActiveWorkspace, setActiveWorkspace } =
     useWorkspaceStore();
+  const { setDocuments, setIsLoadingDocuments } = useDocumentStore();
 
   const [content, setContent] = useState<Content | null>(null);
 
   useEffect(() => {
     if (!params.slug) return;
 
-    const fetchWorkspaceBySlug = async () => {
+    const initializeWorkspaceData = async () => {
       try {
         const workspace = await workspaceService.getWorkspaceBySlug(
           params.slug,
         );
+        const docs = await workspaceDocumentService.getDocumentsByWorkspaceId(workspace.id);
+        setDocuments(docs);
         setActiveWorkspace(workspace);
       } catch (error) {
         console.error(error);
-        toast.error("Error fetching current workspace", {
+        toast.error("Error fetching current workspace data", {
           position: "top-center",
         });
       } finally {
         setIsLoadingActiveWorkspace(false);
+        setIsLoadingDocuments(false);
       }
     };
-    fetchWorkspaceBySlug();
+    initializeWorkspaceData();
   }, [params.slug]);
 
   return (
