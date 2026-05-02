1c6dea6acb2dff10296aa3456fb455d339944c90
Narayan Pal <narayan220602@gmail.com>
2026-05-02 12:25:20 +0530

    log slug in layout of workspace/[slug] route

--- src/app/(protected)/workspace/[slug]/layout.tsx
+++ src/app/(protected)/workspace/[slug]/layout.tsx
@@ -1,5 +1,7 @@
 import SidebarComponent from "@/components/Sidebar/SidebarComponent";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children, params }: { children: React.ReactNode, params: Promise<{slug: string}> }) {
+  const { slug } = await params;
+  console.log("slug ", slug)
   return <SidebarComponent>{children}</SidebarComponent>;
 }
