a0b44ee04ec43bbf741b19764234bf599795827b
Narayan Pal <narayan220602@gmail.com>
2026-05-03 17:01:37 +0530

    create workspace provider to access current workspace data and documents

---
+++ src/components/workspaces/workspace-provider.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Documents, Workspaces } from "@/lib/supabase/models";
+import { createContext, useContext } from "react";
+
+type WorkspaceContextType = {
+  workspace: Workspaces;
+  documents: Partial<
+    Pick<Documents, "id" | "title" | "workspace_id" | "updated_at">
+  >[];
+};
+
+const WorkspaceContext = createContext<WorkspaceContextType | null>(null);
+
+export function WorkspaceProvider({
+  workspace,
+  documents,
+  children,
+}: {
+  workspace: Workspaces;
+  documents: Partial<
+    Pick<Documents, "id" | "title" | "workspace_id" | "updated_at">
+  >[];
+  children: React.ReactNode;
+}) {
+  return (
+    <WorkspaceContext.Provider value={{ workspace, documents }}>
+      {children}
+    </WorkspaceContext.Provider>
+  );
+}
+
+export function useWorkspace() {
+  const context = useContext(WorkspaceContext);
+
+  if (!context) {
+    throw new Error("useWorkspace must be used within WorkspaceProvider");
+  }
+
+  return context;
+}
