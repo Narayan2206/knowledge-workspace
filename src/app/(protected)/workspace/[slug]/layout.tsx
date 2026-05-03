ac9b8c8a20e3539260f55553dd281ca541014570
Narayan Pal <narayan220602@gmail.com>
2026-05-03 17:00:31 +0530

    fetch workspace and documents on server side

--- src/app/(protected)/workspace/[slug]/layout.tsx
+++ src/app/(protected)/workspace/[slug]/layout.tsx
@@ -1,7 +1,29 @@
 import SidebarComponent from "@/components/Sidebar/SidebarComponent";
+import { WorkspaceProvider } from "@/components/workspaces/workspace-provider";
+import { workspaceDocumentService, workspaceService } from "@/lib/services";
+import { Workspaces, Documents } from "@/lib/supabase/models";
+import { getServerSupabase } from "@/lib/supabase/server";
 
-export default async function Layout({ children, params }: { children: React.ReactNode, params: Promise<{slug: string}> }) {
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
-  console.log("slug ", slug)
-  return <SidebarComponent>{children}</SidebarComponent>;
+  const supabase = await getServerSupabase();
+  console.log("slug ", slug);
+  const workspace = await workspaceService.getWorkspaceBySlug(supabase, slug);
+  const documents = await workspaceDocumentService.getDocumentsByWorkspaceId(
+    supabase,
+    workspace.id,
+  );
+
+  console.log("DOCUMENTS AND WORKSPACE SERVER", workspace, documents)
+  return (
+    <WorkspaceProvider workspace={workspace} documents={documents}>
+      <SidebarComponent>{children}</SidebarComponent>
+    </WorkspaceProvider>
+  );
 }
