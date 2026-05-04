3d8ed9d4e46d85068913cd433a64aefdc5e7bbe9
Narayan Pal <narayan220602@gmail.com>
2026-05-04 22:08:14 +0530

    implement realtime document title update in sidebar

--- src/app/(protected)/workspace/[slug]/document/[docId]/DocumentEditor.tsx
+++ src/app/(protected)/workspace/[slug]/document/[docId]/DocumentEditor.tsx
@@ -4,41 +4,49 @@ import Tiptap from "@/components/Editor/Tiptap";
 import { workspaceDocumentService } from "@/lib/services";
 import { getClientSupabase } from "@/lib/supabase/client";
 import { Documents } from "@/lib/supabase/models";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function DocumentEditor({ document }: { document: Documents }) {
   const [title, setTitle] = useState(document.title);
   const [content, setContent] = useState(document.content);
   const supabase = getClientSupabase();
+  const router = useRouter();
   console.log("DOCUMENT CLIENT ", document);
 
   const saveDocument = async () => {
-  try {
-    await workspaceDocumentService.updateDocument(
-      supabase,
-      document.id,
-      {
-        title,
+    try {
+      const payload = {
+        title: title.length === 0 ? "Untitled Document" : title,
         content,
-      },
-      document.workspace_id
-    );
-  } catch (err) {
-    console.error(err);
-  }
-};
+      };
+      await workspaceDocumentService.updateDocument(
+        supabase,
+        document.id,
+        payload,
+        document.workspace_id,
+      );
+      router.refresh();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
-  const timeout = setTimeout(() => {
-    saveDocument();
-  }, 1000);
+    const timeout = setTimeout(() => {
+      saveDocument();
+    }, 1000);
 
-  return () => clearTimeout(timeout);
-}, [title, content]);
+    return () => clearTimeout(timeout);
+  }, [title, content]);
 
   return (
     <div>
-      <CustomInput value={title} onChange={setTitle} />
+      <CustomInput
+        value={title}
+        onChange={setTitle}
+        placeholder="Untitled Document"
+      />
 
       <Tiptap content={content} onChange={setContent} />
     </div>
