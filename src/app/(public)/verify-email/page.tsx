d80250d3b5978396bdff2a838595fd0926c87da5
Narayan Pal <narayan220602@gmail.com>
2026-05-17 21:07:25 +0530

    wrap verifyEmail inside suspense in server component

--- src/app/(public)/verify-email/page.tsx
+++ src/app/(public)/verify-email/page.tsx
@@ -1,9 +1,13 @@
 import VerifyEmail from "@/components/auth/verify-email";
+import { Spinner } from "@/components/ui/spinner";
+import { Suspense } from "react";
 
 export default function VerifyEmailPage() {
   return (
     <main className="h-screen flex items-center justify-center px-4">
-      <VerifyEmail />
+      <Suspense fallback={<Spinner />}>
+        <VerifyEmail />
+      </Suspense>
     </main>
   );
 }
