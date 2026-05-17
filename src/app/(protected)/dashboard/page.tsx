106d2706f28a1ff7eff81da7664a3f05080ee0a6
Narayan Pal <narayan220602@gmail.com>
2026-05-17 20:30:30 +0530

    add logout button in dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,15 @@
+import LogoutButton from "@/components/auth/logout-button";
 import DashboardComponent from "./DashboardComponent";
 
 const Dashboard = () => {
   return (
-    <main className="h-screen">
+    <main className="relative h-screen">
+      <header className="absolute top-0 right-0 p-4">
+        <LogoutButton variant="outline" />
+      </header>
       <div className="flex items-center justify-center h-full">
-      <DashboardComponent />
-    </ div>
+        <DashboardComponent />
+      </div>
     </main>
   );
 };
